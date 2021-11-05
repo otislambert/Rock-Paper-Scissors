04769e1093f4ce0a1dc4e1a1a56b805d1fdacdf6
Otis Lambert <otislambert@gmail.com>
2021-11-05 14:32:51 -0700

    set up most dom manipulation, need to get the actual game working now

--- scripts/app.js
+++ scripts/app.js
@@ -1,56 +1,73 @@
-const statusText = document.querySelector('#status-text');
+const statusText = document.querySelector('#play-status-text');
 statusText.textContent = "Make your play"
+
+const compStatusText = document.querySelector('#comp-status-text');
+
+const winner = document.querySelector('#winner');
+const winnerText = document.querySelector('#winner-status-text');
+
+const playStatus = Array.from(document.querySelectorAll('.plays'));
 
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
-const selectionButton = document.querySelector('.select');
+const selectionButton = Array.from(document.querySelectorAll('.select'));
+const playbtn = document.querySelector('#playbutton');
 
-let userAnswer = "";
+selectionButton.forEach(selectionButton => selectionButton.addEventListener('click', plays))
 
-function setUserAnswer() {
-	let userAnswer = rock;
-	statusText.textContent = `You chose ${userAnswer}!`;
-}
-
-rock.addEventListener('click', setUserAnswer());
-
+let userAnswer = "hi";
+let computerAnswer = "";
 const options = ["rock", "paper", "scissors"];
 
-// function computerPlay() {
-// 	let selector = Math.floor(Math.random() * 3);
-// 	return options[selector];
-// }
+function plays(e) {
+	setUserAnswer(e);
+	computerPlay();
+	setTimeout(playRound, 3000);
+}
 
-// function playRound() {
-// 	let computerAnswer = computerPlay();
-// 	statusText.textContent = `You chose: ${userAnswer}, the computer chose: ${computerAnswer}`;
-// 	if (comparator(userAnswer) == computerAnswer) {
-// 		console.log("You Win");
-// 		return true
-// 	}
-// 	else if (comparator(computerAnswer) == userAnswer) {
-// 		console.log("Computer Wins");
-// 		return false
-// 	}
-// 	else {
-// 		console.log("Tie Game");
-// 	}
-// }
+function setUserAnswer(e) {
+	statusText.textContent = (`You have chosen ${e.target.id}.`)
+	userAnswer = e.target.id
+}
 
-// function comparator(answer) {
-// 	if (answer == "rock") {
-// 		return "scissors";
-// 	}
-// 	else if (answer == "paper") {
-// 		return "rock";
-// 	}
-// 	else if (answer == "scissors") {
-// 		return "paper";
-// 	} else {
-// 		alert("this doesn't beat anything");
-// 	}
-// }
+function computerPlay() {
+	let selector = Math.floor(Math.random() * 3);
+	answer = options[selector];
+	compStatusText.textContent = `The Computer chose ${answer}`;
+	return answer;
+}
+
+function playRound() {
+	console.log(playStatus);
+	playStatus.forEach((Element) => {Element.classList.add('hidden')});
+	console.log(winner);
+	winner.classList.remove('hidden');
+	if (comparator(userAnswer) == computerAnswer) {
+		winnerText.textContent = "You Win!";
+		return true;
+	}
+	else if (comparator(computerAnswer) == userAnswer) {
+		winnerText.textContent = "The Computer Wins.";
+		return false;
+	}
+	else {
+		winnerText.textContent = "It's a tie game."
+	}
+}
+
+function comparator(answer) {
+	if (answer == "rock") {
+		return "scissors";
+	}
+	else if (answer == "paper") {
+		return "rock";
+	}
+	else if (answer == "scissors") {
+		return "paper";
+	} else {
+	}
+}
 
 // function game() {
 // 	let userWins = 0;
