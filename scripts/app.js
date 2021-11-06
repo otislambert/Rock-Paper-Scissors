8a4af4b8ea41694c34d72c327d5d2fb14f778644
otis <otislambert@gmail.com>
2021-11-06 15:19:07 -0700

    game is working now, odd behavior at the end of the game though

--- scripts/app.js
+++ scripts/app.js
@@ -15,22 +15,19 @@ const scissors = document.querySelector('#scissors');
 const selectionButton = Array.from(document.querySelectorAll('.select'));
 const playbtn = document.querySelector('#playbutton');
 
-selectionButton.forEach(selectionButton => selectionButton.addEventListener('click', plays))
-
 let userAnswer = "";
 let computerAnswer = "";
 
 let userWins = 0;
 let computerWins = 0;
-let playCount = 0;
+let plays = 1;
+
+let roundWinner = "";
 
 const options = ["rock", "paper", "scissors"];
 
-function plays(e) {
-	setUserAnswer(e);
-	computerPlay();
-	setTimeout(playRound, 2000);
-	setTimeout(reset, 7000);
+if (plays <= 6) {
+	selectionButton.forEach(selectionButton => selectionButton.addEventListener('click', game))
 }
 
 function setUserAnswer(e) {
@@ -50,21 +47,20 @@ function computerPlay() {
 function playRound() {
 	playStatus.forEach((Element) => {Element.classList.add('hidden')});
 	winnerDiv.classList.remove('hidden');
-	playCount += 1;
 	if (comparator(userAnswer) == computerAnswer) {
 		winnerText.textContent = "You Win!";
-		userWins += 1;
 		setTimeout(displayScore, 2000);
+		roundWinner = 'user';
 		return true;
 	}
 	else if (comparator(computerAnswer) == userAnswer) {
 		winnerText.textContent = "The Computer Wins.";
-		computerWins += 1;
 		setTimeout(displayScore, 2000);
+		roundWinner = 'computer';
 		return false;
 	}
 	else {
-		winnerText.textContent = "It's a tie game."
+		winnerText.textContent = 'It\'s a tie game.';
 	}
 }
 
@@ -92,32 +88,36 @@ function reset() {
 	statusText.textContent = "Make your play."
 }
 
-// function game() {
-// 	let userWins = 0;
-// 	let computerWins = 0;
-// 	let plays = 0;
-// 	while (plays < 6) {
-// 		playRound()
-// 		plays += 1;
-// 		if (playRound()) {
-// 			userWins += 1;
-// 		}
-// 		else {
-// 			computerWins += 1;
-// 		}
-// 		console.log(`The score is now Computer: ${computerWins}, User: ${userWins}. On round ${plays} out of 5.`)
-// 	}
-// 	determineWinner(userWins, computerWins)
-// }
+function game(e) {
+	console.log(plays)
+	if (plays <= 5) {
+		setUserAnswer(e);
+		computerPlay();
+		setTimeout(playRound, 2000);		
+		plays += 1;
+		if (roundWinner == 'user') {
+			userWins += 1;
+			setTimeout(displayScore, 4000);
+			setTimeout(reset, 6000);
+		}
+		else if (roundWinner == 'computer') {
+			computerWins += 1;
+			setTimeout(displayScore, 4000);
+			setTimeout(reset, 6000);
+		}
+		return;
+	}
+	determineWinner();
+}
 
-// function determineWinner(user, computer) {
-// 	if (user > computer) {
-// 		alert("You win. Refresh to play again.")
-// 	}
-// 	else if (computer > user) {
-// 		alert("Computer Wins. Refresh to play again.")
-// 	}
-// 	else {
-// 		alert("Tie Game. Refresh to play again.")
-// 	}
-// }
+function determineWinner() {
+	if (userWins > computerWins) {
+		statusText.textContent = "You win. Refresh to play again.";
+	}
+	else if (computerWins > userWins) {
+		statusText.textContent = "Computer Wins. Refresh to play again.";
+	}
+	else {
+		statusText.textContent = "Tie Game. Refresh to play again.";
+	}
+}
